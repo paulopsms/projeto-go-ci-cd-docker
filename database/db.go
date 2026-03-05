5e7a91dee4d4a8b359c22b180f95e314958f5486
paulopsms <paulopsms@gmail.com>
2026-03-04 23:09:53 -0300

    test ssl require

--- database/db.go
+++ database/db.go
@@ -20,7 +20,7 @@ func ConectaComBancoDeDados() {
     name := os.Getenv("DB_NAME")
     port := os.Getenv("DB_PORT")
 
-    stringDeConexao := "host="+ host + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=disable"
+    stringDeConexao := "host="+ host + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=require"
 // 	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
