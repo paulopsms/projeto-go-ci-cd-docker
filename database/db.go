1d0df22b04626e96eaedce10ae47a82d3d5edd50
paulopsms <paulopsms@gmail.com>
2026-03-28 18:08:15 -0300

    log message

--- database/db.go
+++ database/db.go
@@ -24,7 +24,7 @@ func ConectaComBancoDeDados() {
 // 	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panic("Erro ao conectar com banco de dados: %v", err)
 	}
 
 	DB.AutoMigrate(&models.Aluno{})
